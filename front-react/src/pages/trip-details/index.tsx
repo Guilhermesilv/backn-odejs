3b4ae4f4fa4799dea2cd66314465d46b3fd27174
Guilherme <guilherme@upik.com.br>
2025-07-10 18:33:32 -0300

    commit fix

--- front-react/src/pages/trip-details/index.tsx
+++ front-react/src/pages/trip-details/index.tsx
@@ -46,7 +46,7 @@ export function TripDetailPage() {
         <main className='flex gap-16 px-4'>
           <div className='flex-1 space-y-6'>
             <div className='flex items-center justify-between'>
-              <h2 className='text-3xl font-semibold'>Atividades</h2>
+              <h2 className='text-3xl font-semibold text-zinc-100'>Atividades</h2>
               <Button variant='primary' size='default' onClick={openCreateActivityModal}>
                 <Plus className='size-5' />
                 Cadastrar atividade
